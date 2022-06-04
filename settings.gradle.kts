c3c4c569ee191275216e2785d9fb21bb1e1a6220
flikas <flikas@outlook.com>
2022-06-04 21:03:16 +0800

    Fix: build plugin failed.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.intellij") version "1.5.3"
+        id("org.jetbrains.intellij") version "1.6.0"
         id("org.jetbrains.changelog") version "1.3.1"
         id("io.freefair.lombok") version "6.4.3"
     }
